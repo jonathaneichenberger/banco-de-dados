a5e67534cdc4c27e9e3d29997139655e0e9e7897
Jonathan Eichenberger <161581454+jonathaneichenberger@users.noreply.github.com>
2024-08-21 15:37:57 -0300

    Update sistema_hospital.sql

--- sistema_hospital/sistema_hospital.sql
+++ sistema_hospital/sistema_hospital.sql
@@ -1,2 +1,19 @@
 -- criando a database
-create database sistema_hospital;
+create database if not exists sistema_hospital;
+
+-- selecionando database para adicionar as tables na sequencia
+use sistema_hospital;
+
+-- criando a table paciente
+create table if not exists paciente(
+id_paciente int primary key auto_increment,
+nome varchar(100) not null,
+cpf varchar(14) not null unique,
+data_nascimento date not null,
+endereco varchar(255),
+telefone varchar(15) not null,
+sexo char(1) not null,
+email varchar(100) unique
+);
+
+
